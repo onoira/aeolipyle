4e58364ed61e256414ea8b11631f0fca2b4472c6
Onoira <onoira@psiko.zone>
2023-03-19 06:30:43 +0100

    chore: Simplify cleanup script

--- clean.ps1
+++ clean.ps1
@@ -1,12 +1,8 @@
 $VERBOSE=$false
 Remove-Item -Verbose:$VERBOSE scripts/download_workshop_items.steamcmd -ErrorAction SilentlyContinue
-if (!(Test-Path -Path fids)) {
-    New-Item -Verbose:$VERBOSE -Path fids -ItemType File
-} else {
+if (Test-Path -Path fids) {
     Clear-Content -Verbose:$VERBOSE -Path fids
 }
-if (!(Test-Path -Path urls)) {
-    New-Item -Verbose:$VERBOSE -Path urls -ItemType File
-} else {
+if (Test-Path -Path urls) {
     Clear-Content -Verbose:$VERBOSE -Path urls
 }
