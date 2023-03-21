3d43c2708b53eecbc180be102de0465cdd3b3a61
Onoira <onoira@psiko.zone>
2023-03-21 23:49:20 +0100

    refactor: Remove unused appid from steamcmd runner

--- run.ps1
+++ run.ps1
@@ -1,4 +1,3 @@
 $SCRIPT = "scripts\download_workshop_items.steamcmd"
-$appid = Get-Content -First 1 settings\app.id
 $steamcmd = Get-Content settings\steamcmd.location
 & "$steamcmd\steamcmd.exe" +runscript $(Get-ChildItem "$SCRIPT").FullName
